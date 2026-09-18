cb36e806e9824c7304d778a281884455b1c823b5
Sophia8226 <sophia8226@163.com>
2026-09-18 13:27:47 +0200

    Update this project

--- tools/pdf.R
+++
@@ -1,9 +0,0 @@
-library(usethis)
-library(devtools)
-
-document()
-
-
-devtools::build_manual(
-  pkg  = "D:/SS2026/Master Thesis/priceDiscoveRy",
-  path = "D:/SS2026/Master Thesis/priceDiscoveRy")
